2b8d39ffb32319a09ac255bfae5d7cbc5bf783a6
MsssOlivia <34927420+MsssOlivia@users.noreply.github.com>
2019-03-16 20:04:24 -0700

    Update binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,14 +3,14 @@ int binary_search(int A[], int key, int len) {
   int low=0;
   int high=len-1;
   while(low<=high){
-  int mid=low+(high-low)/2;
-  if (A[mid]==key) return mid;
-  if(A[mid]>key) {
-    high=mid-1;
-  }
-  if(A[mid]<key) {
-    low =mid+1;
-  }
+    int mid=low+(high-low)/2;
+    if (A[mid]==key) return mid;
+    if(A[mid]>key) {
+       high=mid-1;
+    }
+    if(A[mid]<key) {
+      low =mid+1;
+    }
   }
   return -1;
 }
